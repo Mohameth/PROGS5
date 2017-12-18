3aae09707dee001091b957470495e3002b02e1b4
moussadel <32848530+moussadel@users.noreply.github.com>
2017-12-18 22:22:44 +0100

    Add files via upload

---
+++ arm_simulator-1.4/memory_2.c
@@ -0,0 +1,213 @@
+/*
+Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Copyright (C) 2011 Guillaume Huard
+Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
+termes de la Licence Publique Générale GNU publiée par la Free Software
+Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+
+Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+GARANTIE, ni explicite ni implicite, y compris les garanties de
+commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
+Licence Publique Générale GNU pour plus de détails.
+
+Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
+temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+États-Unis.
+
+Contact: Guillaume.Huard@imag.fr
+	 Bâtiment IMAG
+	 700 avenue centrale, domaine universitaire
+	 38401 Saint Martin d'Hères
+*/
+#include <stdlib.h>
+#include "memory.h"
+#include "util.h"
+
+struct memory_data {
+
+	size_t size;
+	uint32_t *memo;	
+	int is_big_endian;
+};
+/*----------------------------------------------------------------------------------------------*/
+
+memory memory_create(size_t size, int is_big_endian) {
+    
+	memory mem = malloc(sizeof( memory)) ;
+	mem->memo = malloc(sizeof(uint32_t)*size);
+	mem->size= size;
+	mem->is_big_endian = is_big_endian;
+return mem;
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+size_t memory_get_size(memory mem) {
+
+
+	return mem->size;
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+void memory_destroy(memory mem) { 
+	free(mem->memo);
+	free(mem);
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
+	
+	if (address < 0 || address > sizeof(uint8_t)*mem->size)
+	        return -1;
+	*value = mem->memo[address] & (255);/*2^8-1*/
+	return 0;
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+int memory_read_half(memory mem, uint32_t address, uint16_t *value) {
+ 
+	if (address < 0 || address > sizeof(uint8_t)*mem->size)
+	        return -1;
+	*value = mem->memo[address] & (65535);/*2^16-1*/
+	return 0;   
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
+
+	if (address < 0 || address > sizeof(uint32_t)*mem->size)
+	        return -1;
+	*value = mem->memo[address];
+	return 0;
+}
+
+/*----------------------------------------------------------------------------------------------*/
+uint8_t reverse_endianess_8_bit(uint8_t value){
+  
+	uint8_t resultat = 0;
+	char *source, *destination;
+	int i;
+
+	source = (char *) &value;
+	destination = ((char *) &resultat)+sizeof(uint8_t);
+	for (i=0; i<sizeof(uint8_t); i++)
+		*(--destination) = *(source++);
+	return resultat;
+}
+
+int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
+
+	if (address < 0 || address > sizeof(uint8_t)*mem->size)
+	        return -1;
+	if (mem->is_big_endian) {
+        	mem->memo[address] = value;
+    	} else {
+        value = reverse_endianess_8_bit(value);
+        mem->memo[address] = value;
+    	}
+	return mem->memo[address] == value;
+}    
+
+/*----------------------------------------------------------------------------------------------*/
+
+uint16_t reverse_endianess_16_bit(uint16_t value)
+{
+  uint16_t resultat = 0;
+  char *source, *destination;
+  int i;
+
+  source = (char *) &value;
+  destination = ((char *) &resultat)+sizeof(uint16_t);
+  for (i=0; i<sizeof(uint16_t); i++)
+      *(--destination) = *(source++);
+  return resultat;
+}
+int memory_write_half(memory mem, uint32_t address, uint16_t value) {
+
+	if (address < 0 || address > sizeof(uint16_t)*mem->size)
+	        return -1;
+	if (mem->is_big_endian) {
+        	mem->memo[address] = value;
+    	} else {
+        value = reverse_endianess_16_bit(value);
+        mem->memo[address] = value;
+    	}
+    
+	return mem->memo[address] == valu
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+uint32_t reverse_endianess_32_bit(uint32_t value)
+{
+  uint32_t resultat = 0;
+  char *source, *destination;
+  int i;
+
+  source = (char *) &value;
+  destination = ((char *) &resultat)+sizeof(uint32_t);
+  for (i=0; i<sizeof(uint32_t); i++)
+      *(--destination) = *(source++);
+  return resultat;
+}
+int memory_write_word(memory mem, uint32_t address, uint32_t value) {
+ 	 
+	if (address < 0 || address > sizeof(uint32_t)*mem->size)
+	        return -1;
+	if (mem->is_big_endian) {
+        	mem->memo[address] = value;
+    	} else {
+        value = reverse_endianess_32_bit(value);
+        mem->memo[address] = value;
+    	}
+    
+	return mem->memo[address] == value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
